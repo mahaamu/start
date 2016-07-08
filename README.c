2951c3206faee42ce1b1c2f58e74d4c955d56dbd
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 11:29:41 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# start
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("nnumber ispositive");
+else
+printf("number is negative");
+}
+}
